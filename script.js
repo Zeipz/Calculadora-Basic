40bd4a7a10203ca30750aa2154f27d139904ca96
Zeip21 <elianzambrano522@gmail.com>
2024-06-03 10:55:03 -0500

    Sumar

---
+++ script.js
@@ -0,0 +1,15 @@
+function obtenerValores() {
+    const numero1 = parseFloat(document.getElementById('numero1').value);
+    const numero2 = parseFloat(document.getElementById('numero2').value);
+    return { numero1, numero2 };
+}
+
+function mostrarResultado(operacion, resultado) {
+    document.getElementById('resultado').innerText = `Resultado de la ${operacion}: ${resultado}`;
+}
+
+function sumar() {
+    const { numero1, numero2 } = obtenerValores();
+    const resultado = numero1 + numero2;
+    mostrarResultado('suma', resultado);
+}
